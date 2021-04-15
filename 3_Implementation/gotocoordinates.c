bb79766c0e2bacffd61f861b28c3d914692442ee
anasinikitha <67320294+anasinikitha@users.noreply.github.com>
2021-04-15 12:37:48 +0530

    Create gotocoordinates.c

---
+++ 3_Implementation/gotocoordinates.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<windows.h>
+COORD coordinates= {0,0};
+void gotocoordinate(int x,int y)
+{
+    coordinates.X=x;
+    coordinates.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coordinates);
+}
